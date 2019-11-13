65c4b8e968ba63a4b1d3054dbc31ff5ae9b9e999
elPerro72 <raymond.de.hond@gmail.com>
2019-11-13 20:28:20 +0000

    changed main

--- src/main/java/com/kiktan/kafkaclient/KafkaClient.java
+++ src/main/java/com/kiktan/kafkaclient/KafkaClient.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class KafkaClient {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaClient.class, args);
 	}
 
 }
