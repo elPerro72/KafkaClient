ee0964f7ee06cd0ce3c8ee51f61e7e76c079f43b
elPerro72 <raymond.de.hond@gmail.com>
2019-11-13 21:40:14 +0000

    added kafka code

--- src/main/java/com/kiktan/kafkaclient/KafkaClient.java
+++ src/main/java/com/kiktan/kafkaclient/KafkaClient.java
@@ -1,13 +1,39 @@
 package com.kiktan.kafkaclient;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class KafkaClient {
+public class KafkaClient implements CommandLineRunner{
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaClient.class, args);
-	}
+    }
+    
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+    private final CountDownLatch latch = new CountDownLatch(3);
+
+    @Override
+    public void run(String...args) throws Exception {
+        this.template.send("myTopic", "foo1");
+        this.template.send("myTopic", "foo2");
+        this.template.send("myTopic", "foo3");
+        latch.await(60, TimeUnit.SECONDS);
+    }
+
+    @KafkaListener(topics = "myTopic")
+    public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+        latch.countDown();
+    }
 
 }
